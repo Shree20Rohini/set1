f6691b3827fb29e5578a270155e0ec154d1f2b8c
Shree Rohini.I <shreerohini98@gmail.com>
2019-02-04 00:40:08 -0800

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int n,r=0,t;
+scanf("%d",&n);
+t=n;
+while(t != 0)
+{
+  r= r * 10;
+  r= r + t%10;
+  t= t/10;
+}
+if(n==r)
+printf("yes");
+else
+printf("no");
+return 0;
+}
