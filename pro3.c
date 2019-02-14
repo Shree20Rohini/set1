4cf010e9decf2e981d5e9a2ddee7af4617bafad0
Shree Rohini.I <shreerohini98@gmail.com>
2019-02-14 16:36:31 +0530

    Create pro3.c

---
+++ pro3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char a[10],b[10];
+int i,j,c=0;
+scanf("%s %s",a,b);
+for(i=0,j=0;a[i]!='\0',b[j]!='\0';i++,j++)
+{
+if(a[i]!=b[j])
+{
+c++;
+}
+}
+printf("%d",c);
+return 0;
+}
