2bc197cd54bf46e1f235ef1125c80566ba633c4e
Shree Rohini.I <shreerohini98@gmail.com>
2019-02-23 08:24:43 -0800

    Create findreverse.c

---
+++ findreverse.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+int n;
+scanf("%d",&n);
+int arr[n],max,i;
+for(i=0;i<n;i++)
+scanf("%d",&arr[i]);
+max=arr[0];
+for(i=0;i<n;i++)
+{
+if(max<arr[i])
+max=arr[i];
+}
+max=max+1;
+int b[max];
+for(i=0;i<max;i++)
+b[i]=0;
+for(i=0;i<n;i++)
+b[arr[i]]+=1;
+for(i=0;i<max;i++)
+{
+if(b[i]==1)
+printf("%d",i);
+}
+return 0;
+}
